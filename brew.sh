4031e1982c4c4327fb3d69279237179591ed6a71
Fai Fung <ffung@xebia.com>
2015-02-26 12:27:10 +0100

    add virtualbox and vagrant

--- brew.sh
+++ brew.sh
@@ -58,7 +58,8 @@ brew install macvim-split-browser
 
 # Install casks
 brew install caskroom/cask/brew-cask
-brew cask info vagrant
+brew cask install virtualbox
+brew cask install vagrant
 brew cask install --appdir="/Applications" caffeine
 brew cask install --appdir="/Applications" clipmenu
 brew cask install --appdir="/Applications" firefox
