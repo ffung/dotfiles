e9b1c3257b52df2c748f8ef30c6dbccd0124dde1
Fai Fung <ffung@xebia.com>
2017-03-01 10:16:31 +0100

    cleanup and add iterm profiles

--- brew.sh
+++ brew.sh
@@ -8,6 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+brew analytics off
+
 # Make sure we're using the latest Homebrew.
 brew update
 
@@ -17,8 +19,6 @@ brew upgrade
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don't forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
@@ -43,7 +43,6 @@ brew install ssh-copy-id
 brew install jq
 brew install the_silver_searcher
 brew install tmux
-brew install macvim && brew linkapps macvim
 
 # Install some build tooling
 brew install gradle
@@ -62,7 +61,6 @@ brew install docker
 brew install docker-compose
 brew install docker-machine
 brew install docker-swarm
-brew install fleetctl
 brew install kubernetes-cli
 
 brew tap homebrew/binary
@@ -75,29 +73,24 @@ brew cask install java
 brew cask install haskell-platform
 brew cask install xquartz
 brew cask install tunnelblick
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv
+brew cask install --appdir="/Applications" macvim
 brew cask install --appdir="/Applications" caffeine
 brew cask install --appdir="/Applications" clipmenu
-brew cask install --appdir="/Applications" eclipse-java
-brew cask install --appdir="/Applications" evernote
 brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" flux
 brew cask install --appdir="/Applications" iterm2
 brew cask install --appdir="/Applications" libreoffice
 brew cask install --appdir="/Applications" omnifocus
-brew cask install --appdir="/Applications" p4v
 brew cask install --appdir="/Applications" slack
-brew cask install --appdir="/Applications" skype
 brew cask install --appdir="/Applications" sourcetree
 brew cask install --appdir="/Applications" virtualbox
 brew cask install --appdir="/Applications" vitamin-r
-brew cask install --appdir="/Applications" vlc
 brew cask install --appdir="/Applications" vmware-fusion
-brew cask install qlmarkdown # install quick look for markdown files
 
 brew tap caskroom/versions
 brew cask install --appdir="/Applications" omnigraffle-professional5
 
 brew tap homebrew/versions
-
 # Remove outdated versions from the cellar.
 brew cleanup
